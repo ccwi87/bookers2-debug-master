769c80c9d5c9f16e0e46471f535e03a6ab985392
ccwi87 <att.kgh@gmail.com>
2021-01-05 04:28:59 +0000

    [Start]応用課題2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  devise_for :users
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
