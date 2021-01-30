03de185cbfefef47f32ce66c17da07c0413b61f5
ccwi87 <att.kgh@gmail.com>
2021-01-30 05:28:14 +0000

    いいね機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ApplicationRecord
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+
+  has_many :favorites, dependent: :destroy
+
+  def already_favorited?(book)
+    self.favorites.exists?(book_id: book.id)
+  end
+
 end
